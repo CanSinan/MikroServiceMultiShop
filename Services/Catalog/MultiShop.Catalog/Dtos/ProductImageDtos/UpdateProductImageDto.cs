04f80ffa4b0f9a0b14350bcda027a26286b2bd6a
Sinan Can <Sinan51can@gmail.com>
2024-03-27 15:53:05 +0300

    Bölüm 2 : MongoDb için settingsler yazıldı. Dto'lar yazıldı. Auto Mapper eklendi.

--- Services/Catalog/MultiShop.Catalog/Dtos/ProductImageDtos/UpdateProductImageDto.cs
+++ Services/Catalog/MultiShop.Catalog/Dtos/ProductImageDtos/UpdateProductImageDto.cs
@@ -1,12 +1,11 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿namespace MultiShop.Catalog.Dtos.ProductImageDtos
 {
-    public class ProductImages
+    public class UpdateProductImageDto
     {
-        public string ProductImagesID { get; set; }
+        public string ProductImageID { get; set; }
         public string Image1 { get; set; }
         public string Image2 { get; set; }
         public string Image3 { get; set; }
         public string ProductId { get; set; }
-        public Product Product { get; set; }
     }
 }
