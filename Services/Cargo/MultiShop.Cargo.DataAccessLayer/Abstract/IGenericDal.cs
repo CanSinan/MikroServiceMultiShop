d0f3195eba1b1c907b4e5fbcbafdc26d12a493d0
Sinan Can <Sinan51can@gmail.com>
2024-04-08 16:46:54 +0300

    +++ Bölüm 12 :  Cargo Mikroservisi + Kargo Mikroservisi oluşturuldu.(N katmanlı mimari kullanıldı.) Kargo Entityleri Eklendi. + CargoContext Sınıfı oluşturuldu. + Kargo Veri Tabanı Migration İşlemi Yapıldı. +Cargo Generic Dal ve Interfaceler eklendi. + Generic Repository Projede uygulandı.(Rpository Design pattern) + Ef Sınıfları eklendi. + Business Katmanı Interfaceleri yazıldı. +Business katmanı Manager Sınıfları yazıldı. + Cargo Company Controller yazıldı. + Cargo Customer Controller yazıldı. + Cargo Detail Controller Yazıldı. + Cargo Operation Controller yazıldı ve Registiration işlemleri yapıldı. + Api testi yapıldı. + Cargo Mikroservisi token ile korundu.

---
+++ Services/Cargo/MultiShop.Cargo.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using MultiShop.Cargo.DataAccessLayer.Concrete;
+using MultiShop.Cargo.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Cargo.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        T GetById(int id);
+        List<T> GetAll();
+    }
+}
